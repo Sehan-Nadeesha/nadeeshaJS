7d19a6d027d66564c726cd7b62cca3f409211ce5
Nadeesha <nadeeshamunasingha898@gmail.com>
2024-11-25 12:00:27 +0530

    incrementing-count

---
+++ 50 Projects/Incrementing_Counter/script.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+  counter.innerText = '0';
+  const updateCount = () => {
+    const target = +counter.getAttribute('data-target');
+    const count = +counter.innerText;
+    const increment = target / 200;
+    if (count < target) {
+      counter.innerText = `${Math.ceil(count + increment)}`;
+      setTimeout(updateCount, 1);
+    } else {
+      counter.innerText = target
+    }
+  }
+  updateCount();
+
+});
